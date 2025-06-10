b5c4f8c90b6f3190ae9473f3fd2da94aed469ecc
ravindra gausvami <goswamirvi@gmail.com>
2025-06-10 20:24:04 +0530

    feat: Add Google Ads integration to home page

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -134,4 +134,16 @@
       </div>
     </div>
   </section>
+  <section>
+    <!-- unit1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5560225028494268"
+     data-ad-slot="7534125021"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+  </section>
   @endsection
