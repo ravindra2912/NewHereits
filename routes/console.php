11bae63ffc767a88f9eed50fd539eb2c370e9830
ravindra gausvami <goswamirvi@gmail.com>
2025-07-07 18:51:58 +0530

    fix: Comment out redundant scheduling for appointment auto-cancel command

--- routes/console.php
+++ routes/console.php
@@ -11,13 +11,13 @@ Artisan::command('inspire', function () {
 
 Log::info("Run schedule at " . date('y-m-d-h-i-s'));
 
-Schedule::call(function () {
-    Log::info('This job runs every minute or more'); 
-})->everySecond()->onFailure(function () {
-    Log::error('This job failed to run at ' . now());
-});
+// Schedule::call(function () {
+//     Log::info('This job runs every minute or more'); 
+// })->everySecond()->onFailure(function () {
+//     Log::error('This job failed to run at ' . now());
+// });
 
-Schedule::command('app:appoinment-auto-cancel')->everySecond();
+// Schedule::command('app:appoinment-auto-cancel')->everySecond();
 Schedule::command('app:appoinment-auto-cancel')->dailyAt('00:01');
 Schedule::command('queue:work --stop-when-empty')->everySecond();
 
