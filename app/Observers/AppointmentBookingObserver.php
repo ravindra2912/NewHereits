cce588441e30f3a415b6464a09b02584162fc3b5
ravindra gausvami <goswamirvi@gmail.com>
2025-07-14 17:56:36 +0530

    fix: Update debug statement to output appointmenter details in created event

--- app/Observers/AppointmentBookingObserver.php
+++ app/Observers/AppointmentBookingObserver.php
@@ -31,7 +31,7 @@ class AppointmentBookingObserver
             ])
             ->find($appointmentBooking->id);
 
-            dd($appointment_details->appointmenter->is_appointment_book_with_time_slot);
+            dd($appointment_details->appointmenter);
 
         if ($appointment_details && $appointment_details->user_id != null) {
             if ($appointment_details->appointmenter->is_appointment_book_with_time_slot) {
