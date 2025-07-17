42392940afd0a4095414ae7f2400174fe6c8ff62
ravindra gausvami <goswamirvi@gmail.com>
2025-07-17 13:57:45 +0530

    $businesses->isEmpty()

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -40,8 +40,7 @@ class HomeController extends Controller
         }
         $businesses = $businesses->where('status', 'active')->limit(8)->get();
 
-        dd($userLocationInfo['radius'], $businesses, $businesses->isEmpty(), $userLocationInfo['radius'] == 5);
-        if (!$businesses && $userLocationInfo['radius'] == 5) { 
+        if ($businesses->isEmpty() && $userLocationInfo['radius'] == 5) { 
             $userLocationInfo['radius'] = 15;
             $updatedData = [
                 'data' => $userLocationInfo,
