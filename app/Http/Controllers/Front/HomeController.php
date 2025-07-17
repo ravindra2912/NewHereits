f91e2a4aa24e1b6aa9c2159dba76a2b2d562ffc6
ravindra gausvami <goswamirvi@gmail.com>
2025-07-17 13:52:53 +0530

    session('hereitsLocation')

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -47,6 +47,7 @@ class HomeController extends Controller
                 'expires_at' => now()->addDays(7),
             ];
             session()->put('hereitsLocation', $updatedData);
+            dd(session('hereitsLocation'));
             goto retry;
         }
 
