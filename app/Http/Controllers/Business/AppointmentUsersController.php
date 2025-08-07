7c088d5c28de5b978494077bde37edb65edb008c
ravindra gausvami <goswamirvi@gmail.com>
2025-08-07 13:14:40 +0530

    count issue

--- app/Http/Controllers/Business/AppointmentUsersController.php
+++ app/Http/Controllers/Business/AppointmentUsersController.php
@@ -39,9 +39,8 @@ class AppointmentUsersController extends Controller
                     return '<div class="text-center"><img src="' . getImage($row->profile) . '" class="table_img" /></div>';
                 })
                 ->addColumn('convertRate', function ($row) {
-                    return $row->uncompleted_appointments > 0
-                        ? round(($row->completed_appointments / $row->uncompleted_appointments) * 100, 2) . '%'
-                        : '0%';
+                    $total = $row->completed_appointments + $row->uncompleted_appointments;
+                    return $total > 0 ? round(($row->completed_appointments / $total) * 100, 2) : '0' . '%';
                 })
                 ->addColumn('action', function ($row) {
                     return ' <div class="text-center">
