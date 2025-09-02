b03d3e4d16ef15d1a701871bd43cc9c17defcb63
ravindra gausvami <goswamirvi@gmail.com>
2025-09-02 17:25:04 +0530

    Update navbar links to use route helper for navigation

--- resources/views/business/layouts/main.blade.php
+++ resources/views/business/layouts/main.blade.php
@@ -58,9 +58,9 @@
           <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <!-- <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
+          <a href="{{ route('home') }}" class=" btn btn-info ">Go to Front</a>
+        </li> -->
+        <!-- li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
       </li> -->
       </ul>
@@ -68,10 +68,14 @@
       <!-- Right navbar links -->
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
+          <a href="{{ route('home') }}" class=" btn btn-info ">Go to Front</a>
+        </li>
+        
+        <!-- <li class="nav-item">
           <a class="nav-link" data-widget="fullscreen" href="#" role="button">
             <i class="fas fa-expand-arrows-alt"></i>
           </a>
-        </li>
+        </li> -->
       </ul>
     </nav>
     <!-- /.navbar -->
